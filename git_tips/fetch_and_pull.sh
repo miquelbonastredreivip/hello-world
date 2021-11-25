3cd36d4debf427eaa5757149c8a06096292af50e
Miquel Bonastre <miquel.bonastre@dreivip.com>
2021-11-25 12:26:12 +0100

    Add git script to fetch and pull

---
+++ git_tips/fetch_and_pull.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#
+# Mantains current repo updated with changes in origin
+#
+# If some step fails because of some conflict, the process stops.
+#
+# Si algun pas falla per algun conflicte, el procés s'atura.
+#
+# Passos:
+# - Fetch
+# - Pull current branch (it could be ${MAIN})
+# - Pull from origin ${MAIN} to local ${MAIN}
+#
+
+# Choose your player:
+#MAIN="main"
+MAIN="master"
+
+date &&
+  git status &&
+  echo FETCH: && git fetch &&
+  echo PULL current branch: && git pull &&
+  echo PULL ${MAIN}: && git pull origin ${MAIN}:${MAIN}
+
+if [ "$?" != 0 ] ; then
+  echo "ERROR: something went wrong"
+  exit 1
+fi
+
