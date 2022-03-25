dc550eab5be8b2edf70991a01adcc0e275da0b2f
Miquel Bonastre <miquel.bonastre@dreivip.com>
2022-03-25 14:38:14 +0100

    Fix spelling error in safe

--- git_tips/safe_push_to_master.sh
+++ git_tips/safe_push_to_master.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
-
+#
+# @author: https://github.com/mbonastre
+#
 #
 # Push branch changes to ${MAIN} using Fast-Forward
 #
 # If some step fails because of some conflict, the process stops.
 #
-# Steps:
-#  - ToDo
+# Current branch should not be main (first pull will fail)
 #
+# Steps:
+#  - Make sure local-main is up-to-date with origin-main (fetch and pull)
+#  - Stash non-staged local changes, if applicable.
+#  - Rebase
+#  - Push local-branch changes to origin-branch and origin-main
+#  - 
 # Fem "push" de canvis en una branca cap a ${MAIN}, usant Fast-Forward
 #
 # Si algun pas falla per algun conflicte, el procés s'atura.
 #
-# Passos:
+# Passos:
 # - Actualitzem info local i remot (fetch/pull)
 # - Ens assegurem que tenim els darrers canvis (rebase)
 # - Apliquem canvis (push) des d'aquesta branca (HEAD) cap a ${MAIN} remot
@@ -24,21 +31,20 @@
 MAIN="master"
 
 date &&
-echo "ACTUALITZEM LOCAL REPO:" &&
-  echo fetch: && git fetch --prune &&
-  echo pull ${MAIN}: && git pull origin ${MAIN}:${MAIN} &&
+echo "UPDATE LOCAL REPO WITH LATEST CHANGES:" &&
+  echo "fetch:" && git fetch --prune &&
+  echo "pull origin-${MAIN} to local-${MAIN}:" && git pull origin ${MAIN}:${MAIN} &&
 
 echo "REBASE FROM ${MAIN}:" &&
   echo stash: && git stash &&
   echo rebase: && git rebase ${MAIN} && 
 
-echo "PUSHES:" &&
-  echo push-branch:      &&  git push &&
-  echo push-to-${MAIN}:  &&  git push origin HEAD:${MAIN} &&
+echo "PUSHES OUR CHANGES:" &&
+  echo "push local-branch to origin-branch:" &&  git push &&
+  echo "push local-branch to origin-${MAIN}:"  &&  git push origin HEAD:${MAIN} &&
 
-echo "ACTUALITZEM LOCAL ${MAIN}:" &&
-  echo pull-${MAIN} && git pull origin ${MAIN}:${MAIN}
-
+echo "PULL OUR CHANGES TO LOCAL ${MAIN}:" &&
+  echo "pull-${MAIN}" && git pull origin ${MAIN}:${MAIN}
 
 echo "DON'T FORGET !!!!! Do you need to do some of these? "
 
@@ -51,6 +57,6 @@ echo "  git fetch && git pull"
 echo "Branch cleaning:"
 echo " - delete local : git branch -d BRANCH"
 echo " - delete remote: git push origin --delete BRANCH"
-echo " - ignore remote: git branch --unset-upstream [BRANCH]
+echo " - ignore remote: git branch --unset-upstream [BRANCH]"
 echo " - delete tracking: git remote prune origin"
 
